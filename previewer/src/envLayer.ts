9dcd28e00f7a6f9c0382f402dfd1b264667b5d3a
leesungbin <lee@sungbin.dev>
2023-04-15 02:00:13 +0900

    Apply oauth bot token

--- previewer/src/envLayer.ts
+++ previewer/src/envLayer.ts
@@ -6,4 +6,4 @@ export const PREFIX = process.env.PREFIX ?? 'preview';
 export const GENERATE_PERIOD = process.env.GENERATE_PREIOD ?? '518400000';
 export const JUBOT_AUTHORIZATION = process.env.JUBOT_AUTHORIZATION ?? '';
 export const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET ?? '';
-export const SLACK_USER_OAUTH_TOKEN = process.env.SLACK_USER_OAUTH_TOKEN ?? '';
+export const SLACK_BOT_OAUTH_TOKEN = process.env.SLACK_BOT_OAUTH_TOKEN ?? '';
--- previewer/src/slack.ts
+++ previewer/src/slack.ts
@@ -1,20 +1,21 @@
 import { createEventAdapter } from '@slack/events-api';
-import { SLACK_SIGNING_SECRET, SLACK_USER_OAUTH_TOKEN } from './envLayer';
+import { SLACK_SIGNING_SECRET, SLACK_BOT_OAUTH_TOKEN } from './envLayer';
 import { WebClient } from '@slack/web-api';
 
 const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET);
 
-const client = new WebClient(SLACK_USER_OAUTH_TOKEN);
+const client = new WebClient(SLACK_BOT_OAUTH_TOKEN);
 
 slackEvents.on('message', (event) => {
   const { user, text, ts: timestamp, channel } = event;
+  console.log(JSON.stringify(event));
   client.chat.postMessage({
-    token: SLACK_USER_OAUTH_TOKEN,
+    token: SLACK_BOT_OAUTH_TOKEN,
     channel,
     text: text,
   });
   client.reactions.add({
-    token: SLACK_USER_OAUTH_TOKEN,
+    token: SLACK_BOT_OAUTH_TOKEN,
     channel,
     name: 'thumbsup',
     timestamp,
