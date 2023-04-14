39f8daeea8267708b683eddafc4fdaf815bfb400
leesungbin <lee@sungbin.dev>
2023-04-15 01:52:39 +0900

    Apply token for slack response

--- previewer/src/slack.ts
+++ previewer/src/slack.ts
@@ -2,18 +2,23 @@ import { createEventAdapter } from '@slack/events-api';
 import { SLACK_SIGNING_SECRET, SLACK_USER_OAUTH_TOKEN } from './envLayer';
 import { WebClient } from '@slack/web-api';
 
-const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET, {
-  includeBody: true,
-  includeHeaders: true,
-});
+const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET);
 
 const client = new WebClient(SLACK_USER_OAUTH_TOKEN);
 
 slackEvents.on('message', (event) => {
-  console.log('event received', JSON.stringify(event));
   const { user, text, ts: timestamp, channel } = event;
-  client.chat.postMessage({ channel, text: text });
-  client.reactions.add({ channel, name: 'thumbsup', timestamp });
+  client.chat.postMessage({
+    token: SLACK_USER_OAUTH_TOKEN,
+    channel,
+    text: text,
+  });
+  client.reactions.add({
+    token: SLACK_USER_OAUTH_TOKEN,
+    channel,
+    name: 'thumbsup',
+    timestamp,
+  });
 });
 
 export { slackEvents };
