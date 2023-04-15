c21eaf42e298708e5c4b0bf0fd4d242a0834daa2
leesungbin <lee@sungbin.dev>
2023-04-15 11:38:13 +0900

    Apply file upload

--- previewer/src/slack.ts
+++ previewer/src/slack.ts
@@ -3,29 +3,72 @@ import {
   SLACK_SIGNING_SECRET,
   SLACK_BOT_OAUTH_TOKEN,
   SLACK_BOT_ID,
+  JUBOT_URL,
 } from './envLayer';
 import { WebClient } from '@slack/web-api';
+import { isEmpty } from 'lodash';
+import { saveScreenshot } from './preview';
+import fs from 'fs/promises';
+import path from 'path';
 
 const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET);
 
 const client = new WebClient(SLACK_BOT_OAUTH_TOKEN);
+const previewRules = [/preview/g, /주보/g];
 
-slackEvents.on('message', (event) => {
+slackEvents.on('message', async (event) => {
   const { user, text, ts: timestamp, channel } = event;
   if (user === SLACK_BOT_ID) return;
 
   console.log(JSON.stringify(event));
-  client.chat.postMessage({
-    token: SLACK_BOT_OAUTH_TOKEN,
-    channel,
-    text: text,
-  });
+
+  // TODO: match for every rules
+  const matched = !isEmpty(previewRules.filter((rule) => rule.test(text)));
+  if (matched) {
+    client.reactions.add({
+      token: SLACK_BOT_OAUTH_TOKEN,
+      channel,
+      name: 'thumbsup',
+      timestamp,
+    });
+    const filename = await saveScreenshot(JUBOT_URL);
+    const file = await fs.readFile(path.join(__dirname, '..', filename));
+    const uplaoded = await client.filesUploadV2({
+      token: SLACK_BOT_OAUTH_TOKEN,
+      channel_id: channel,
+      file,
+      filename: filename.split('/').slice(-1).toString(),
+    });
+    if (uplaoded.ok) {
+      client.reactions.add({
+        token: SLACK_BOT_OAUTH_TOKEN,
+        channel,
+        name: 'white_check_mark',
+        timestamp,
+      });
+    }
+    return;
+  }
+
+  // client.chat.postMessage({
+  //   token: SLACK_BOT_OAUTH_TOKEN,
+  //   channel,
+  //   text: text,
+  // });
+
   client.reactions.add({
     token: SLACK_BOT_OAUTH_TOKEN,
     channel,
-    name: 'thumbsup',
+    name: 'question',
     timestamp,
   });
 });
+
+class Rule {
+  constructor(public regex: RegExp) {}
+  match(text: string) {
+    return this.regex.test(text);
+  }
+}
 
 export { slackEvents };
