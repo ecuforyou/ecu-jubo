9687bf3728faa9fed3c21a5b00ee8fb016d232f1
leesungbin <lee@sungbin.dev>
2023-05-01 23:17:04 +0900

    Upload test

--- previewer/src/slack/client.ts
+++ previewer/src/slack/client.ts
@@ -19,11 +19,18 @@ export class SlackClient extends WebClient {
       const file = await fs.readFile(
         path.join(__dirname, '..', '..', filename)
       );
-      await this.filesUploadV2({
+      console.log('filename:', filename);
+      await this.files.uploadV2({
         token: SLACK_BOT_OAUTH_TOKEN,
         channel_id: channel,
         file,
-        filename: filename.split('/').slice(-1).toString(),
+        filename: 'v2' + filename.split('/').slice(-1).toString(),
+      });
+      await this.files.upload({
+        token: SLACK_BOT_OAUTH_TOKEN,
+        channel_id: channel,
+        file,
+        filename: 'v1' + filename.split('/').slice(-1).toString(),
       });
     } catch (err) {
       console.log(JSON.stringify(err));
